7c8f762b7a6ad75474fe7106e8775d193163c30a
alexlenehan06-cell <alexlenehan06@gmail.com>
2026-03-30 14:17:23 +0100

    Delete api.js

--- api.js
+++
@@ -1,9 +0,0 @@
-fetch("https://jsonplaceholder.typicode.com/users")
-.then(res=>res.json())
-.then(data=>{
- let output="";
- data.slice(0,5).forEach(loc=>{
-  output+=`<p>Gym in ${loc.address.city}</p>`;
- });
- document.getElementById("locations").innerHTML=output;
-});
